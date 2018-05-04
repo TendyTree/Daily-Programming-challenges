469661adf11a2b7c0cccf416a5713e0c776a30c9
xxaxun1t <hockeyfan2@live.com>
2018-05-04 16:35:28 -0400

    Finished

--- [2018-04-30] Challenge #359 [Easy] Regular Paperfold Sequence Generator/Test.py
+++ [2018-04-30] Challenge #359 [Easy] Regular Paperfold Sequence Generator/Test.py
@@ -1,1 +1,8 @@
-test
+array = [1]
+repeat = int(input("Please enter how many time you would like it to repeat:"))
+for a in range(repeat):
+    for b in range(1,len(array)+2):
+        y = b % 2
+        x = (b * 2) - 2
+        array.insert(x,y)
+    print(array)
