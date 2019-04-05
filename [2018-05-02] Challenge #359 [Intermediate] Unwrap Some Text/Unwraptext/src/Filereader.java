45f6d33196e18e730ff53cd34ee6b883e247a254
xxaxun1t <hockeyfan2@live.com>
2019-04-06 00:58:18 +0100

    Finished challange

--- [2018-05-02] Challenge #359 [Intermediate] Unwrap Some Text/Unwraptext/src/Filereader.java
+++ [2018-05-02] Challenge #359 [Intermediate] Unwrap Some Text/Unwraptext/src/Filereader.java
@@ -1,49 +1,54 @@
 import java.util.Scanner;
 import java.io.*;
-public class Filereader extends Paragraph {
-	Filereader(int newLines, int newAvgchar) {
-		super(newLines, newAvgchar);
+public class Filereader{
+	Filereader() {
 		// TODO Auto-generated constructor stub
 	}
-
-	public  void read(String path) throws Exception{
-		try{
-			File file = new File(path);
-			Scanner f = new Scanner(file);
-			String newString = null;
-			int line = 0;
-			int character = 0;
-			while(f.hasNextLine()){
-				newString = f.nextLine();		
-				if(newString.charAt(newString.length()) != '.' || newString.charAt(newString.length()) != '!'){
-					line++;
-					character += newString.length();
-					}
-				super.setChar(character);
-				super.setLines(line);
-				}
-			}
-		catch(Exception e){
-		throw new Exception(e);	
-		}
-	}
-
 	public void writeoutput(String path) throws Exception{
 	try{
-		PrintWriter writer = new PrintWriter("Output.txt", "UTF-8");
+		PrintWriter writer = new PrintWriter("Output.txt");
 		File file = new File(path);
 		Scanner f = new Scanner(file);
-		String newString = null;
+		String newString;
+		String currentString = "";
 		int maxchar = 120;
-		int line = 0;
 		int character = 0;
 		while(f.hasNextLine()){
-			newString = f.nextLine();			
-			if(newString.charAt(newString.length()) != '.' || newString.charAt(newString.length()) != '!'){
+			newString = f.nextLine();
+			if(newString.charAt(newString.length()-1) != '.' && newString.charAt(newString.length()-1) != '!'){
+				if(character < maxchar){
+					if(newString.length() < (maxchar - character)){
+						currentString += newString;
+						character = currentString.length();
+					}else{
+						while(newString.charAt(maxchar - character) != ' '){
+							character++;
+						}
+					writer.println(currentString + newString.substring(0,(maxchar-character)));
+					currentString = newString.substring(maxchar-character+1, newString.length());
+					character = currentString.length();
+					}
+					
+				}
+			}else{
+				if(newString.length() <= (maxchar - character)){
+					currentString += newString;
+					writer.println(currentString);
+					character = 0;
+					currentString = "";
+				}else{
+					while(newString.charAt(maxchar - character) != ' ' && character != 119){
+						character++;
+					}
+				writer.println(currentString + newString.substring(0,(maxchar-character)));
+				writer.println(newString.substring(maxchar-character+1, newString.length()));
+				character = 0;
+				currentString = "";
+				}
 			}
-		super.setChar(character);
-		super.setLines(line);
 		}
+		writer.close();
+		f.close();
 	}
 	catch(Exception e){
 		throw new Exception(e);	
--- [2018-05-02] Challenge #359 [Intermediate] Unwrap Some Text/Unwraptext/src/Unwraptext.java
+++ [2018-05-02] Challenge #359 [Intermediate] Unwrap Some Text/Unwraptext/src/Unwraptext.java
@@ -7,11 +7,11 @@ public class Unwraptext {
 		try{
 			Scanner scan = new Scanner (System.in);
 			String newString;
-			System.out.println("please enter address:This program does not work");
+			System.out.println("please enter address:");
 			newString = scan.nextLine();
-			Filereader filereader = new Filereader(0,0);
-			filereader.read(newString);
-			
+			Filereader filereader = new Filereader();
+			filereader.writeoutput(newString);
+			System.out.println("exit");
 		}
 		catch(Exception e){
 		System.out.println("There was an error");
